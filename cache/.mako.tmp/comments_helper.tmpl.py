2179ddaa381fa5dcdd19096942ad2756f51b23b5
Ivelin <ivelina@rapidmathematix.com>
2018-02-03 12:17:22 -0800

    Nikola auto commit.
    Nikola version: 7.8.11

--- cache/.mako.tmp/comments_helper.tmpl.py
+++ cache/.mako.tmp/comments_helper.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1517688511.5841532
+_modified_time = 1517689041.810265
 _enable_loop = True
 _template_filename = '/usr/local/lib/python3.6/site-packages/nikola/data/themes/base/templates/comments_helper.tmpl'
 _template_uri = 'comments_helper.tmpl'
@@ -65,11 +65,11 @@ def render_body(context,**pageargs):
 def render_comment_form(context,url,title,identifier):
     __M_caller = context.caller_stack._push_frame()
     try:
+        livefyre = _mako_get_namespace(context, 'livefyre')
         muut = _mako_get_namespace(context, 'muut')
         intensedebate = _mako_get_namespace(context, 'intensedebate')
-        livefyre = _mako_get_namespace(context, 'livefyre')
-        googleplus = _mako_get_namespace(context, 'googleplus')
         facebook = _mako_get_namespace(context, 'facebook')
+        googleplus = _mako_get_namespace(context, 'googleplus')
         comment_system = context.get('comment_system', UNDEFINED)
         isso = _mako_get_namespace(context, 'isso')
         disqus = _mako_get_namespace(context, 'disqus')
@@ -111,11 +111,11 @@ def render_comment_form(context,url,title,identifier):
 def render_comment_link(context,link,identifier):
     __M_caller = context.caller_stack._push_frame()
     try:
+        livefyre = _mako_get_namespace(context, 'livefyre')
         muut = _mako_get_namespace(context, 'muut')
         intensedebate = _mako_get_namespace(context, 'intensedebate')
-        livefyre = _mako_get_namespace(context, 'livefyre')
-        googleplus = _mako_get_namespace(context, 'googleplus')
         facebook = _mako_get_namespace(context, 'facebook')
+        googleplus = _mako_get_namespace(context, 'googleplus')
         comment_system = context.get('comment_system', UNDEFINED)
         isso = _mako_get_namespace(context, 'isso')
         disqus = _mako_get_namespace(context, 'disqus')
@@ -157,13 +157,13 @@ def render_comment_link(context,link,identifier):
 def render_comment_link_script(context):
     __M_caller = context.caller_stack._push_frame()
     try:
-        muut = _mako_get_namespace(context, 'muut')
-        intensedebate = _mako_get_namespace(context, 'intensedebate')
         livefyre = _mako_get_namespace(context, 'livefyre')
-        googleplus = _mako_get_namespace(context, 'googleplus')
-        facebook = _mako_get_namespace(context, 'facebook')
-        comment_system = context.get('comment_system', UNDEFINED)
         isso = _mako_get_namespace(context, 'isso')
+        intensedebate = _mako_get_namespace(context, 'intensedebate')
+        facebook = _mako_get_namespace(context, 'facebook')
+        googleplus = _mako_get_namespace(context, 'googleplus')
+        comment_system = context.get('comment_system', UNDEFINED)
+        muut = _mako_get_namespace(context, 'muut')
         disqus = _mako_get_namespace(context, 'disqus')
         __M_writer = context.writer()
         __M_writer('\n')
--- cache/.mako.tmp/list.tmpl.py
+++ cache/.mako.tmp/list.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1517688511.481369
+_modified_time = 1517689042.1046112
 _enable_loop = True
 _template_filename = '/usr/local/lib/python3.6/site-packages/nikola/data/themes/base/templates/list.tmpl'
 _template_uri = 'list.tmpl'
@@ -39,10 +39,10 @@ def render_body(context,**pageargs):
         archive_nav = _mako_get_namespace(context, 'archive_nav')
         items = _import_ns.get('items', context.get('items', UNDEFINED))
         title = _import_ns.get('title', context.get('title', UNDEFINED))
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         def content():
             return render_content(context._locals(__M_locals))
         messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer('\n')
@@ -66,10 +66,10 @@ def render_content(context,**pageargs):
         archive_nav = _mako_get_namespace(context, 'archive_nav')
         items = _import_ns.get('items', context.get('items', UNDEFINED))
         title = _import_ns.get('title', context.get('title', UNDEFINED))
+        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         def content():
             return render_content(context)
         messages = _import_ns.get('messages', context.get('messages', UNDEFINED))
-        feeds_translations = _mako_get_namespace(context, 'feeds_translations')
         __M_writer = context.writer()
         __M_writer('\n<article class="listpage">\n    <header>\n        <h1>')
         __M_writer(filters.html_escape(str(title)))
